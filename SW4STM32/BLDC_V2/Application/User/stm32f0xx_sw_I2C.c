9e41dd074bbdf4e056b69a587f68e3233e2d36c0
Holloway, Adam Scott <aholloway31@gatech.edu>
2016-07-14 02:32:32 -0400

    Added the I2C delay functions as well as basic read functions for SDA/SCL

--- SW4STM32/BLDC_V2/Application/User/stm32f0xx_sw_I2C.c
+++ SW4STM32/BLDC_V2/Application/User/stm32f0xx_sw_I2C.c
@@ -8,13 +8,57 @@
  */
 
 #include "stm32f0xx_sw_I2C.h"
+#include "stm32f0xx.h"
 #include "gpio.h"
 
-//Delay function for executing i2c operations
-void I2C_DELAY(void){ // Max 400 kbps baud rate = 2.5 us per operation -> minimum of 1.25us delay between operations (rising/falling)
 
+// -------- DELAY FUNCTIONS --------
 
-
-
+// Intialize the SysTick register and set each tick to 1us
+void SysTickInit(uint32_t frequency) {
+	SysTick_Config(SystemCoreClock / 1000000);
 }
 
+//Update sys_ticks counter (defined as a volatile variable)
+static __IO uint32_t sysTickCounter;
+void SysTick_Handler(void) {
+	if (sysTickCounter != 0x00) {
+		sysTickCounter--;
+	}
+}
+// set counter to desired delay and wait for it to reach 0 (handler will count it down based on the tick interrupt)
+void delay(uint32_t us) {
+	sysTickCounter = us;
+	while (sysTickCounter != 0x00) {
+
+	}
+}
+
+void I2C_DELAY(void){ // delay for 2 us (~250Kbps baud rate)
+	delay(2);
+}
+// -------- END DELAY FUNCTIONS --------
+
+
+// -------- I2C FUNCTIONS --------
+
+// Return 8 bit integer for value of SCL (for applying bitwise operators)
+uint8_t READ_SCL(uint16_t SCL) { // Eventually do some magic here to determine the GPIO port register from the function input (pin)
+	GPIO_PinState SCL_State = HAL_GPIO_ReadPin(GPIOF, SCL);
+	if (SCL_State == GPIO_PIN_SET) {
+		return 0x01;
+	}
+	else {
+		return 0x00;
+	}
+}
+// Return 8 bit integer for value of SDA
+uint8_t READ_SDA(uint16_t SDA) {
+	GPIO_PinState SDA_State = HAL_GPIO_ReadPin(GPIOF, SDA);
+	if (SDA_State == GPIO_PIN_SET) {
+		return 0x01;
+	}
+	else {
+		return 0x00;
+	}
+}
--- SW4STM32/BLDC_V2/Application/User/stm32f0xx_sw_I2C.h
+++ SW4STM32/BLDC_V2/Application/User/stm32f0xx_sw_I2C.h
@@ -16,22 +16,20 @@
 
 // define some initial variables */
 //#define	uint32_t I2C_SPEED; // Not sure if I need this here yet (probably going to end up removing it)
-#define I2C_SELF_ADDRESS ((uint8_t) 0xA2);
+//#define I2C_SELF_ADDRESS ((uint8_t) 0xA2);
 
-// Declare delay functions for I2C timing (sourced from code authored by Patrick Leyman)
+// Declare delay functions for I2C timing (based on SysTick interrupt code from Patrick Leyman as well as Peter Harrison)
 void I2C_DELAY(void);
-void SysTickInit(uint32_t sysclk);
-void delay(uint32_t us);
-volatile uint32_t ticks;
+void SysTickInit(uint32_t frequency);
 void SysTick_Handler(void);
+void delay(uint32_t us);
 // End delay functions
 
-
 // Declare base I2C functions
-GPIO_PinState READ_SCL(GPIO_TypeDef* SCL); // Return 8 bit integer for current value of SCL
-GPIO_PinState READ_SDA(GPIO_TypeDef* SDA); // Return 8 bit integer for current value of SDA
-void SET_SDA(GPIO_TypeDef* SDA); 	   // Drive SDA high
-void CLEAR_SDA(GPIO_TypeDef* SDA);   // Drive SDA low
+uint8_t READ_SCL(uint16_t SCL); // Return 8 bit integer for current value of SCL
+uint8_t READ_SDA(uint16_t SDA); // Return 8 bit integer for current value of SDA (maybe GPIO_PinState if I need it)
+void SET_SDA(uint16_t SDA); 	   // Drive SDA high
+void CLEAR_SDA(uint16_t SDA);   // Drive SDA low
 // End core functions
 
 // Declare I2C operations
@@ -41,6 +39,7 @@ void I2C_WRITE_BIT(uint8_t BIT);
 void I2C_WRITE_BYTE(uint8_t BYTE);
 uint8_t I2C_READ_BIT(void);
 uint8_t I2C_READ_BYTE(void);
+
 
 
 #endif /* APPLICATION_USER_STM32F0XX_SW_I2C_H_ */
